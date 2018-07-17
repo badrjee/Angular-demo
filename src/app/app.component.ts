6a522d6dfe839278e83da923f383becb4f0a9d9e
badrjee <badro_87@yahoo.com>
2018-07-17 10:24:23 +0200

    ajout des fonctionalités

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Hero } from './hero';
+import { NgForm } from '../../node_modules/@Angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour Of Heroes';
+
+  title : string;
+  newHeroName : string;
+  @ViewChild(HeroesComponent)
+  HeroesComponent:HeroesComponent;
+
+  constructor(){
+    this.title = 'Tour Of Heroes';
+    this.newHeroName='';
+
+  }
+
+  validateHero(event: any, form:NgForm){
+    let hero : Hero = new Hero();
+    hero.name = this.newHeroName;
+    this.HeroesComponent.heroes.push(hero);
+    form.resetForm();
+  }
+ 
 }
